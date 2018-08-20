aba503d4918f8cdb4d29ac1e1e8184d55ecfcf17
Andrei Kniazev <akniazev@gmail.com>
2018-08-20 13:03:02 +0000

    Bind awsd buttons to move the figure on screen

--- moving_shape.py
+++ moving_shape.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import time
 
 
 class myWindow():
@@ -10,6 +9,26 @@ class myWindow():
         self.canvas.pack()
         self.canvas.create_rectangle(130, 100, 170, 120, fill="purple")
         self.canvas.create_text(150, 20, text="Use A, W, S, D to move")
+        self.root.bind("a", self.moveLeft)
+        self.root.bind("d", self.moveRight)
+        self.root.bind("w", self.moveUp)
+        self.root.bind("s", self.moveDown)
+
+    def moveRight(self, event):
+        self.canvas.move(1, 10, 0)
+        self.root.update()
+
+    def moveLeft(self, event):
+        self.canvas.move(1, -10, 0)
+        self.root.update()
+
+    def moveUp(self, event):
+        self.canvas.move(1, 0, -10)
+        self.root.update()
+
+    def moveDown(self, event):
+        self.canvas.move(1, 0, 10)
+        self.root.update()
 
 
 window = myWindow()
