a5cf505c9cfdc72b1ff715938e6782d207b3fbfb
Andrei Kniazev <akniazev@gmail.com>
2018-08-20 12:25:34 +0000

    Change window to a class

--- moving_shape.py
+++ moving_shape.py
@@ -1,10 +1,16 @@
 import tkinter as tk
 import time
-root = tk.Tk()
 
-canvas = tk.Canvas(root, width=300, height=300)
-canvas.pack()
-canvas.create_rectangle(130, 100, 170, 120, fill="purple")
-canvas.create_text(150, 20, text="Use A, W, S, D to move")
 
-root.mainloop()
+class myWindow():
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.canvas = tk.Canvas(self.root, width=300, height=300)
+        self.canvas.pack()
+        self.canvas.create_rectangle(130, 100, 170, 120, fill="purple")
+        self.canvas.create_text(150, 20, text="Use A, W, S, D to move")
+
+
+window = myWindow()
+window.root.mainloop()
