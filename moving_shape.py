f3a426a0efa9d135d5dabbb235816048e4becfc5
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-08-20 07:18:14 -0700

    Parametrise location of rectangle to make it centered

--- moving_shape.py
+++ moving_shape.py
@@ -5,10 +5,26 @@ class myWindow():
 
     def __init__(self):
         self.root = tk.Tk()
-        self.canvas = tk.Canvas(self.root, width=300, height=300)
+        self.winWidth = 300
+        self.winHeight = 300
+        self.canvas = tk.Canvas(self.root,
+                                width=self.winWidth,
+                                height=self.winHeight)
         self.canvas.pack()
-        self.canvas.create_rectangle(130, 100, 170, 120, fill="purple")
-        self.canvas.create_text(150, 20, text="Use A, W, S, D to move")
+
+        self.x = self.winWidth / 2
+        self.y = self.winHeight / 2
+        self.w = self.winWidth / 10
+        self.h = self.winHeight / 20
+
+        self.canvas.create_rectangle(self.x - self.w/2,
+                                     self.y - self.h/2,
+                                     self.x + self.w/2,
+                                     self.y + self.h/2,
+                                     fill="purple")
+        self.canvas.create_text(self.winWidth/2, self.winHeight/15,
+                                text="Use A, W, S, D to move")
+
         self.root.bind("a", self.moveLeft)
         self.root.bind("d", self.moveRight)
         self.root.bind("w", self.moveUp)
