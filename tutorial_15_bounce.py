3621e74fbb7b771b34b0b6420f9cf894cbd99cc2
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-08-21 07:53:56 -0700

    Add paddle and controls for it

--- tutorial_15_bounce.py
+++ tutorial_15_bounce.py
@@ -31,15 +31,40 @@ class Ball:
         pos = self.canvas.coords(self.id)   # [x1, y1, x2, y2]
         if pos[1] <= 0 or pos[3] >= self.canvas_height:
             self.y *= -1
-        if pos[0] <= 0 or pos[2] >= self.canvas_height:
+        if pos[0] <= 0 or pos[2] >= self.canvas_width:
             self.x *= -1
 
 
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.move_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.move_right)
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)   # [x1, y1, x2, y2]
+        if pos[0] <= 0 or pos[2] >= self.canvas_width:
+            self.x = 0
+
+    def move_left(self, evt):
+        self.x = -2
+
+    def move_right(self, evt):
+        self.x = 2
+
+
 ball = Ball(canvas, 'red')
+paddle = Paddle(canvas, 'blue')
 
 while 1:
     try:
         ball.draw()
+        paddle.draw()
     except:
         break
     root.update_idletasks()
