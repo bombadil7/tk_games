f4a97a533a56a7bc9edbbe776ecd8af03d845f0d
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-08-21 07:29:19 -0700

    Both x and y are limited and the ball stays on screen

--- tutorial_15_bounce.py
+++ tutorial_15_bounce.py
@@ -19,19 +19,20 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-        # start = [-3, -2, -1, 0, 1, 2, 3]
-        # random.shuffle(start)
-        self.x = 0
-        self.y = -1
+        start = [-3, -2, -1, 0, 1, 2, 3]
+        random.shuffle(start)
+        self.x = start[0]
+        self.y = -3
         self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)   # [x1, y1, x2, y2]
-        if pos[1] <= 0:
-            self.y = 1
-        if pos[3] >= self.canvas_height:
-            self.y = -1
+        if pos[1] <= 0 or pos[3] >= self.canvas_height:
+            self.y *= -1
+        if pos[0] <= 0 or pos[2] >= self.canvas_height:
+            self.x *= -1
 
 
 ball = Ball(canvas, 'red')
