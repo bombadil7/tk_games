efe86095a7946804f09fa39d0c33dd0e8471dab2
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-08-21 07:20:52 -0700

    Add limits so the ball bounces, but stays on screen

--- tutorial_15_bounce.py
+++ tutorial_15_bounce.py
@@ -12,7 +12,6 @@ canvas = tk.Canvas(root, width=500, height=500, bd=0,
 canvas.pack()
 
 root.update()
-#    time.sleep(0.1)
 
 
 class Ball:
@@ -20,15 +19,28 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
+        # start = [-3, -2, -1, 0, 1, 2, 3]
+        # random.shuffle(start)
+        self.x = 0
+        self.y = -1
+        self.canvas_height = self.canvas.winfo_height()
 
     def draw(self):
-        self.canvas.move(self.id, 0, -1)
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)   # [x1, y1, x2, y2]
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.canvas_height:
+            self.y = -1
 
 
 ball = Ball(canvas, 'red')
 
 while 1:
-    ball.draw()
+    try:
+        ball.draw()
+    except:
+        break
     root.update_idletasks()
     root.update()
     time.sleep(0.01)
